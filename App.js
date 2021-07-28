e78dc2bd75ac456dde2b764f139048e0eea55653
Donovan Juca Cupueran <djuca001@gold.ac.uk>
2021-07-28 20:34:15 +0100

    Basic screens added, Drawer added, Basic styling added

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function App() {
+const HomeStack = createStackNavigator();
+const DetailsStack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const HomeStackScreen = ({ navigation }) => (
+  <HomeStack.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor: '#aa80ff'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold'
+    }
+  }}>
+    <HomeStack.Screen name='Home' component={HomeScreen} options={{
+      headerLeft: () => (
+        <Ionicons.Button name='ios-menu' size={35}
+          backgroundColor='#aa80ff' onPress={() => { navigation.openDrawer() }}
+        />
+      )
+    }} />
+  </HomeStack.Navigator>
+);
+
+const DetailsStackScreen = ({ navigation }) => (
+  <DetailsStack.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor: '#aa80ff'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold'
+    }
+  }}>
+    <DetailsStack.Screen name='Details' component={DetailsScreen} options={{
+      headerLeft: () => (
+        <Ionicons.Button name='ios-menu' size={35}
+          backgroundColor='#aa80ff' onPress={() => { navigation.openDrawer() }}
+        />
+      )
+    }} />
+  </DetailsStack.Navigator>
+);
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName='Home'>
+        <Drawer.Screen name='Home' component={HomeStackScreen} />
+        <Drawer.Screen name='Details' component={DetailsStackScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, View, Text } from "react-native";
+const HomeScreen = ({ navigation }) => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                title='Go to Details Screen'
+                onPress={() => navigation.navigate('Details')} />
+        </View>
+    );
+};
+
+export default HomeScreen;

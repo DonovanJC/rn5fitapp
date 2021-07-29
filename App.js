04cce7ed63eddb83fe7a0cf0fe936ce67da36ec0
Donovan Juca Cupueran <djuca001@gold.ac.uk>
2021-07-29 17:57:15 +0100

    Logo Added

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import MainTabScreen from './src/screensFlow/MainTabScreen';
 import { DrawerContent } from './src/screensFlow/DrawerContent';
 import SupportScreen from './src/screens/SupportScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
 import BookmarkScreen from './src/screens/BookmarkScreen';
+import SignStackScreen from './src/screensFlow/SignStackScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -13,12 +15,13 @@ const Drawer = createDrawerNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+      <SignStackScreen />
+      {/* <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
         <Drawer.Screen name='HomeDrawer' component={MainTabScreen} />
         <Drawer.Screen name='Support' component={SupportScreen} />
         <Drawer.Screen name='Settings' component={SettingsScreen} />
         <Drawer.Screen name='Bookmark' component={BookmarkScreen} />
-      </Drawer.Navigator>
+      </Drawer.Navigator> */}
     </NavigationContainer>
   );
 };
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+const SignUpScreen = ({ navigation }) => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Sign Up Screen</Text>
+        </View>
+    );
+};
+
+export default SignUpScreen;

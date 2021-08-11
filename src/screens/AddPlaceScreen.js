45607a18e28a91696ca41c37245eb9cdb53d6332
Donovan Juca Cupueran <djuca001@gold.ac.uk>
2021-08-11 21:04:43 +0200

    Code added to NewPlace Screen, Geocoder Implemented and fully working

--- src/screens/AddPlaceScreen.js
+++ src/screens/AddPlaceScreen.js
@@ -1,27 +1,66 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 import Firebase from "../database/firebase";
+import Geocoder from "react-native-geocoding";
 
 const db = Firebase.firestore();
+Geocoder.init("AIzaSyASYLMRcWSSxC_QbznjGA0hNRkl4-OjE_A");
 
 const AddPlaceScreen = ({ navigation }) => {
 
-    db.collection("users").add({
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
+    const [data, setData] = React.useState({
+        street: "",
+        number: "",
+        postCode: ""
     })
-        .then((docRef) => {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch((error) => {
-            console.error("Error adding document: ", error);
-        });
+
+    let location
+
     return (
         <View>
-            <Text>NEW SCREENSITA</Text>
+            <Text>Add a New Place</Text>
+            <Text>Street</Text>
+            <TextInput
+                placeholder="Enter the street"
+                style={styles.textInput}
+                onChangeText={(val) => setData({ ...data, street: val })}
+            />
+
+            <Text>Number</Text>
+            <TextInput
+                placeholder="Enter the street number"
+                style={styles.textInput}
+                onChangeText={(val) => setData({ ...data, number: val })}
+            />
+
+            <Text>PostCode</Text>
+            <TextInput
+                placeholder="Enter the postcode"
+                style={styles.textInput}
+                onChangeText={(val) => setData({ ...data, postCode: val })}
+            />
+
+            <Button title="GET COORDINATES" onPress={() => {
+                let fulladdress = data.street + " " +  data.number + " " + data.postCode
+                Geocoder.from(fulladdress)
+                    .then(json => {
+                        var location = json.results[0].geometry.location;
+                        console.log(location);
+                    })
+                    .catch(error => console.warn(error));
+            }
+            } />
         </View>
     )
 }
 
 export default AddPlaceScreen;
+
+const styles = StyleSheet.create({
+    // textInput: {
+    //     flex: 1,
+    //     marginTop: Platform.OS === 'ios' ? 0 : -12,
+    //     paddingLeft: 10,
+    //     color: '#05375a',
+    // }
+})
